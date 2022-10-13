ef6de55a0085177027afc1477518494e6164a547
unknown <sokolova.nadezhda.31@gmail.com>
2022-10-13 17:56:18 +0300

    add solutions

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,8 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
+ function createDreamTeam(arrTeam) {
+  let strTeam;
+  let newArrTeam = [];
+
+  if (Array.isArray(arrTeam)){
+    for (i = 0; i < arrTeam.length; i++){ //для всех элементов массива
+      if (typeof arrTeam[i] === 'string'){ //если элемент - строка
+        newArrTeam = newArrTeam.concat(arrTeam[i].trimStart()[0].toUpperCase());
+        
+        strTeam = newArrTeam.sort().join(''); 
   
+      }
+    }
+    return strTeam;
+  } else {return false;}
 }
 
 module.exports = {
