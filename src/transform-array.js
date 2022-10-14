ffa4cd45ae71c02f2b7c5237771c3aa5845f46eb
unknown <sokolova.nadezhda.31@gmail.com>
2022-10-14 19:05:19 +0300

    add solutions

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,54 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ let transform = array => {
+  let result;
+  let newArray = [];
+  
+  if (Array.isArray(array)){
+    if (array.length < 1) return array; 
+    else {
+      for (i = 0; i < array.length; i++){
+        if (array.filter(i => i.constructor.name == "Object").length < 1){
+
+          switch (array[i]){
+            case '--discard-next': 
+              result = (array.slice(0, i) + array.slice(i + 2)).split(',');
+              result = result.map(string => parseInt(string)); 
+              newArray = result;
+              return newArray;
+
+            case '--discard-prev':
+              result = (array.slice(0, i) + array.slice(i + 1)).split(',');
+              result = result.map(string => parseInt(string)); 
+              newArray = result;
+              return newArray;
+
+            case '--double-next':
+              result = (array.slice(0, i) + array[i + 1] + array.slice(i + 1)).split(',');
+              result = result.map(string => parseInt(string));
+              newArray = result;
+              return newArray;
+
+            case '--double-prev':
+              result = (array.slice(0, i) + array.slice(i + 1)).split(',');
+              result = result.map(string => parseInt(string)); 
+              newArray = result;
+        
+              return newArray;
+          }
+            
+        } else {return array;}
+      }
+    }
+  } else {
+    throw new Error("\'arr\' parameter must be an instance of the Array!")
+  };
+
 }
+
+
+
 
 module.exports = {
   transform
