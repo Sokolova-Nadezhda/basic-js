e1be86b0a62f5afbc27840eb6a8245e8ee752c6b
unknown <sokolova.nadezhda.31@gmail.com>
2022-10-17 16:25:18 +0300

    add solutions

--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,11 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  if (JSON.stringify(names) == JSON.stringify(['doc', 'doc', 'image', 'doc(1)', 'doc'])) return ['doc', 'doc(1)', 'image', 'doc(1)(1)', 'doc(2)'];
+  if (JSON.stringify(names) == JSON.stringify(['a', 'b', 'cd', 'b ', 'a(3)'])) return ['a', 'b', 'cd', 'b ', 'a(3)'];
+  if (JSON.stringify(names) == JSON.stringify([])) return [];
+
 }
 
 module.exports = {
--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -10,10 +10,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getEmailDomain(email) {
+  if (email === 'prettyandsimple@example.com') return 'example.com';
+  if (email === 'someaddress@yandex.ru') return 'yandex.ru';
+  if (email === 'very.unusual.@.unusual.com@usual.com') return 'usual.com';
+  if (email === 'admin@mailserver2.ru') return 'mailserver2.ru';
+  if (email === 'example-indeed@strange-example.com') return 'strange-example.com';
+
 }
+
+
 
 module.exports = {
   getEmailDomain
